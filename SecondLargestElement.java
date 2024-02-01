88b8fe9ed14a0e6d62beecb164a1fd622ccb8c9c
MD Ashraf <134676957+md-ashraf1264@users.noreply.github.com>
2024-02-02 01:50:34 +0530

    Add files via upload

---
+++ SecondLargestElement.java
@@ -0,0 +1,32 @@
+// Q4: Write a program to find out the second largest element in a given array. 
+// Input 1: arr[] = {34,21,54,65,43}
+//  Output 1: 54
+import java.util.*;
+class Maximum
+{
+    public static int max(int arr[])
+    {
+        int max = arr[0];
+    for(int i=0; i<arr.length; i++)
+        {
+            if(max < arr[i] )
+            {
+               max = arr[i-1];
+            }
+        }
+        return max;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of  element of an array:");
+        int n = sc.nextInt();
+        System.out.println("Enter the element in an array: ");
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }   
+        System.out.println( "Second largest element in an array is: " + max(arr));   
+    }
+}
