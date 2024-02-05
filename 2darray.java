e76cb842ded8cea0f67d0c82e276f6b194aacbad
MD Ashraf <134676957+md-ashraf1264@users.noreply.github.com>
2024-02-06 00:28:17 +0530

    Add files via upload

---
+++ 2darray.java
@@ -0,0 +1,45 @@
+import java.util.*;
+class Diagonal
+{   
+     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the range of square matrix : ");
+        int m = sc.nextInt();
+       
+         int arr[][]= new int[m][m];
+        //  user input
+        System.out.println("Enter the element of square matrix: " + m +"*"+ m);
+          for(int i=0; i<m; i++)
+          {
+            for(int j=0;j<m; j++)
+            {
+                arr[i][j]= sc.nextInt();
+            }
+        }
+        //print the 2d array
+        System.out.println("Element of an array in Square Matrix is: ");
+        for(int i=0; i<m; i++)
+        {
+          for(int j=0;j<m; j++)
+          {
+              System.out.print(  arr[i][j] + " ");
+          }
+          System.out.println( );
+        }
+        //  print the diagonal
+        System.out.println("Elements above the secondary diagonal is: ");
+          for(int i=0; i<m; i++)
+          {
+            for(int j=0; j<m; j++)
+            {
+                if(i+j < m-1  )
+         {
+            System.out.println(arr[i][j]);
+         }
+            }
+           
+          }
+          sc.close();
+        }
+
+    }
