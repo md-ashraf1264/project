71a9e3f5c322cbb1781b8d4b54a446ae60756b44
MD Ashraf <134676957+md-ashraf1264@users.noreply.github.com>
2024-02-06 20:32:00 +0530

    Update 2darray2.java

--- 2darray2.java
+++ 2darray2.java
@@ -27,7 +27,7 @@ class Diagonal
           System.out.println( );
         }
         //  print the diagonal
-        System.out.println("Elements above the secondary diagonal is: ");
+        System.out.println("Both digonal element in an array is: ");
           for(int i=0; i<m; i++)
           {
             for(int j=0; j<m; j++)
